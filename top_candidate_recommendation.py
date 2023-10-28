b68658dbe39f39b4a486c89b5ad13fc407ead9e3
banelengemntu123 <60341059+banelengemntu123@users.noreply.github.com>
2023-10-28 07:14:28 +0200

    Add files via upload
    Change again

--- top_candidate_recommendation.py
+++ top_candidate_recommendation.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import random
 import numpy as np
-from sentence_transformers import SentenceTransformer
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
